09c5cd8e0752bd136032c2ad8465bb0500636bf4
Koa Lee <koalee@berkeley.edu>
2023-03-12 19:02:53 -0700

    Initial Commit

---
+++ FoodFinder/FoodFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFinderApp.swift
+//  FoodFinder
+//
+//  Created by Koa Lee on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
