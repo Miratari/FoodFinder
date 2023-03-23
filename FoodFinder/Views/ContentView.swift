bf2f92c9f0aba93bdc77cefe215682b9cc5b44a5
Koa Lee <koalee@berkeley.edu>
2023-03-23 00:22:48 -0700

    testing in main view

--- FoodFinder/Views/ContentView.swift
+++ FoodFinder/Views/ContentView.swift
@@ -9,21 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Food Finder")
-                .font(.largeTitle)
-                .foregroundColor(.black)
-            HStack {
-                Text("App for finding good restaurants nearby")
-                    .font(.subheadline)
-                    .foregroundColor(.black)
-                Spacer()
-                Text("Open Project")
-                    .font(.subheadline)
-                    .foregroundColor(.black)
+        
+        ZStack {
+            MapView()
+            //.offset(y: -50)
+            .ignoresSafeArea()
+            ScrollView() {
+                VStack(alignment: .leading) {
+                    Text("Title")
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                    HStack {
+                        Text("Subtitle")
+                            .font(.subheadline)
+                            .foregroundColor(.black)
+                        Spacer()
+                        Text("Open Project")
+                            .font(.subheadline)
+                            .foregroundColor(.black)
+                    }
+                }
+                .offset(y: 100)
             }
+            .ignoresSafeArea()
+            .padding()
         }
-        .padding()
+        
+        
         
     }
 }
