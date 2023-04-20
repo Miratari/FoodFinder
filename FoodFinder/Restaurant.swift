f04a86e85f2edb7f1ca737711c6d4f496b896613
Shrithan Sandadi <shrithan_1@berkeley.edu>
2023-04-19 20:18:11 -0700

    -Added variables to constructor

--- FoodFinder/Restaurant.swift
+++ FoodFinder/Restaurant.swift
@@ -10,6 +10,10 @@ import Foundation
 class Restaurant: Identifiable {
     //instance variables go here
     var name: String
+    var cuisine: String
+    var rating: Double
+    var price_range: String
+    
     let id = UUID()
     
     //class initializers
@@ -17,10 +21,15 @@ class Restaurant: Identifiable {
         name = "Empty"
     }
     
-    init(name new_name: String) {
+    init(name new_name: String, cuisine new_cuisine: String, rating new_rating: Double,
+         price_range new_price_range: String) {
         name = new_name
+        cuisine = new_cuisine
+        rating = new_rating
+        price_range = new_price_range
+        
     }
-    
+
     ///Name
     ///Cuisine
     ///Rating
